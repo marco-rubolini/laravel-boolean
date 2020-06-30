62687f6f638d98e2f5dc52e5c875c75b491bf102
Marco <rubolini.marco@gmail.com>
2020-06-30 16:40:44 +0200

    Installazione Laravel + bootstrap

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Chi siamo</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">Chi siamo</h1>
+                    <p>Ciao {{ $nome }}</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
