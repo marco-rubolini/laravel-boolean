a748b59271a5bd99049467d8f9e4842ca80a2d12
Marco <rubolini.marco@gmail.com>
2020-07-01 11:13:43 +0200

    Pagina privacy Policy + link url

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app');
+@extends('layouts.app')
 @section('content')
     <section id="jumbotron-boolean">
         <div class="jumbotron">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,7 @@
           </button>
           <div class="menu-nav collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
-              <li class="nav-item active">
+              <li class="nav-item {{ Request::route()->getName() == 'homepage' ? 'active' : '' }}">
                 <a class="nav-link boolean text-uppercase" href="{{ route('homepage')}} ">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
